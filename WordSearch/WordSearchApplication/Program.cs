c355a6f399bf273840ed635c3f085d222953bf5d
Jacob Kessler <jacob.kessler@case.edu>
2020-03-24 18:41:23 -0500

    Adding solution and project structure

---
+++ WordSearch/WordSearchApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WordSearchApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
