4e2f3e2f37a3610c502e4da71c77851adb894142
Jacob Kessler <jacob.kessler@case.edu>
2020-03-29 16:42:28 -0500

    Implemented Main method in the WordSearchGamePlayer class

--- WordSearch/WordSearchApplication/WordSearchGamePlayer.cs
+++ WordSearch/WordSearchApplication/WordSearchGamePlayer.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using WordSearchApplication.Models;
 
 namespace WordSearchApplication
 {
@@ -6,9 +10,42 @@ namespace WordSearchApplication
     {
         private static void Main(string[] args)
         {
-            //WordSearchGame game = new WordSearchGame();
+            Console.WriteLine("Hello! My name is Jacob Kessler and this is my implementation of the Word Search Kata.");
+
+            while (true)
+            {
+                Console.WriteLine("Please type a path to your input text file or type 'quit' and press enter: ");
+                string filePath = Console.ReadLine();
+
+                if (filePath == "quit") break;
+                
+                string[] linesFromFile;
+                
+                try
+                {
+                    linesFromFile = File.ReadAllLines(filePath);
+                }
+                catch (DirectoryNotFoundException e)
+                {
+                    Console.WriteLine("I'm sorry, I couldn't find that directory. Message: " + e.Message);
+                    Console.WriteLine("Please try again!");
+                    continue;
+                }
             
-            Console.WriteLine("Hello World!");
+                WordSearchGame game = new WordSearchGame(linesFromFile.ToList());
+            
+                Console.WriteLine("");
+
+                Dictionary<string, List<string>> output = game.GetWordsAndCoordinates();
+
+                foreach (KeyValuePair<string, List<string>> word in output)
+                {
+                    foreach (string coordinate in word.Value)
+                    {
+                        Console.WriteLine(word.Key + ": " + coordinate);
+                    }
+                }
+            }
         }
     }
 }
