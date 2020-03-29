ec92590565896e1e196c63d873b68cec4c3c1b27
Jacob Kessler <jacob.kessler@case.edu>
2020-03-29 13:42:53 -0500

    Created WordSearcher structure and tests with Coordinate object

---
+++ WordSearch/WordSearchApplication/WordSearcher.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using WordSearchApplication.Models;
+
+namespace WordSearchApplication
+{
+    public class WordSearcher
+    {
+        public List<Coordinate> CheckNorth(WordSearchBoard Board, Coordinate origin, string word)
+        {
+            throw new NotImplementedException("CheckNorth not yet implemented");
+        }
+        
+        public List<Coordinate> CheckNorthEast(WordSearchBoard Board, Coordinate origin, string word)
+        {
+            throw new NotImplementedException("CheckNorthEast not yet implemented");
+        }
+        
+        public List<Coordinate> CheckEast(WordSearchBoard Board, Coordinate origin, string word)
+        {
+            throw new NotImplementedException("CheckEast not yet implemented");
+        }
+        
+        public List<Coordinate> CheckSouthEast(WordSearchBoard Board, Coordinate origin, string word)
+        {
+            throw new NotImplementedException("CheckSouthEast not yet implemented");
+        }
+        
+        public List<Coordinate> CheckSouth(WordSearchBoard Board, Coordinate origin, string word)
+        {
+            throw new NotImplementedException("CheckSouth not yet implemented");
+        }
+        
+        public List<Coordinate> CheckSouthWest(WordSearchBoard Board, Coordinate origin, string word)
+        {
+            throw new NotImplementedException("CheckSouthWest not yet implemented");
+        }
+        
+        public List<Coordinate> CheckWest(WordSearchBoard Board, Coordinate origin, string word)
+        {
+            throw new NotImplementedException("CheckWest not yet implemented");
+        }
+        
+        public List<Coordinate> CheckNorthWest(WordSearchBoard Board, Coordinate origin, string word)
+        {
+            throw new NotImplementedException("CheckNorthWest not yet implemented");
+        }
+    }
+}
